1d83d60eda296a6748d14c67804dee9c5147f3ef
Miroslav Juhy Juhos <juhy@rezatyrakety.cz>
2016-06-13 10:32:04 +0200

    Added modules

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,11 +3,17 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
+  beforeModel() {
+    return this.store.findAll('class');
+  },
+
   model() {
-    return this.store.findAll('class').then((data) => {
-      return data.filter((item) => {
+    return {
+      classes: this.store.peekAll('class').filter((item) => {
         return !item.get('parentCls');
-      });
-    });
+      }),
+      modules: this.store.peekAll('module')
+    }
+
   }
 });
